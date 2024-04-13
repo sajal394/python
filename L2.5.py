a09f3521536092e5ee0d54b32e694957f6031279
Sajal <23f2002548@ds.study.iitm.ac.in>
2024-04-13 17:09:00 +0530

    lecture 2.5

---
+++ L2.5.py
@@ -0,0 +1,18 @@
+# Escape characters and types of quotes
+print('It\'s a beautiful day')
+print('We are from "IIT" madras')
+print('My name is Sajal. I am from Karnal.')
+print('My name is Sajal.\tI am from Karnal.')
+print('My name is Sajal.\nI am from Karnal.')
+
+x = 'it is a string'
+y = "it is also a string"
+z = '''first line
+second line
+third line
+fourth line'''
+print(x)
+print(y)
+print(z)
+
+# Triple quotes can be used for commenting also
